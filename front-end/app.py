f9b08a9c3173ab53d872ce351737a0ae2e641b2c
hodangkhoinguyen <50374959+hodangkhoinguyen@users.noreply.github.com>
2023-05-28 12:57:10 -0700

    Save models and run frontend

--- front-end/app.py
+++ front-end/app.py
@@ -1,17 +1,41 @@
 from flask import Flask, render_template, request
-app = Flask('cancer_detection')
+import pandas as pd
+import pickle
+app = Flask(__name__)
 
 @app.route('/')
 def show_predict_stock_form():
     return render_template('predictorform.html')
-@app.route('/results', methods=['POST'])
+@app.route('/predict', methods=['POST'])
+
 def results():
     form = request.form
     if request.method == 'POST':
-      #write your function that loads the model
-      model = get_model() #you can use pickle to load the trained model
-      year = request.form['year']
-      predicted_stock_price = model.predict(year)
-      return render_template('resultsform.html', year=year,   predicted_price=predicted_stock_price)
+      model = pickle.load(open("Logistic-Regression-Model", "rb"))
+      Xlabels = ["radius_mean","texture_mean","smoothness_mean","compactness_mean",
+                 "symmetry_mean","fractal_dimension_mean","radius_se","texture_se",
+                 "smoothness_se","compactness_se",
+                 "symmetry_se",
+                 "radius_worst","texture_worst",
+                 "smoothness_worst","compactness_worst",
+                 "symmetry_worst"]
+      Xtest = [17.99,10.38,0.1184,0.2776,
+               0.2419,0.07871,1.095,0.9053,
+               0.006399,0.04904,
+               0.03003,
+               25.38,17.33,
+               0.1622,0.6656,
+               0.4601]
+      Xdict = {}
+      for i in range(len(Xlabels)):
+        Xdict[Xlabels[i]] = [Xtest[i]]
+      X = pd.DataFrame(data=Xdict)
+      Y = model.predict(X)
+      if Y == 1:
+        cancer_type = "Malignant"
+      else:
+        cancer_type = "Benign"
+
+      return render_template('resultsform.html', cancer_type=cancer_type)
     
 app.run("localhost", "9999", debug=True)
